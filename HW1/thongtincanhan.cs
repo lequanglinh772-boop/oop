56a6fbf03e599e9e0aee32c6a6ebc8ddce97614b
lequanglinh772 <lequanglinh772@gmail.com>
2025-10-02 18:54:29 +0700

    oop12424018

---
+++ HW1/thongtincanhan.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo
+{
+    internal class thongtincanhan
+    {
+            private static void Main(string[] args)
+            {
+
+                string hoTen = "Lưu Hoàng Tùng";
+                string maSV = "12424034";
+                string lop = "124241";
+                string github = "hoangtung-123";
+                string email = "luuhoangtung2710@gmail.com";
+
+
+                Console.WriteLine(hoTen + "\t" + maSV + "\t" + lop + "\t" + github + "\t" + email);
+
+
+                Console.ReadLine();
+            }
+        }
+ }
+
